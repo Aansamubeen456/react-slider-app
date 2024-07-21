8649aa30ce1046f45e155636654bf6bb5fb1a86a
aansamubeen <mubeenansa1@gmail.com>
2024-07-21 18:33:06 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from './Carousel'
+import SlickSlider from './SlickSlider'
+
+const App = () => {
+  return (
+    <main>
+      {/* <Carousel /> */}
+      <SlickSlider />
+    </main>
+  )
+}
+export default App
